28b82d0f2675321a7d8e44c6b71038848a26e492
jgamos <jgamos101@gmail.com>
2017-11-24 07:25:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@
 #Prerequisites
 sudo apt-get update
 
-#FTP
-sudo apt-get install -y proftpd
-
 #Apache
 sudo apt-get install -y apache2
-sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
+
+#Change the persmissions
+sudo chown -R pi:www-data /var/www/html/
+sudo chmod -R 770 /var/www/html/
 
 #PHP
 sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
@@ -23,6 +23,5 @@ sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl
 #MySQL
 sudo apt-get install -y mysql-server mysql-client php5-mysql
 
-#Additional Dependencies
-sudo apt-get install -y nmap zenmap
-
+#Install Phpmyadmin 
+sudo aptitude install -y phpmyadmin
